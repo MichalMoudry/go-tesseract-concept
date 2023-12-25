5ae771e0bcc7e36150f8079923c6f10af7759539
Michal Moudrý <moudry.michal@live.com>
2023-12-25 12:02:48 +0100

    Completed sample

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/otiai10/gosseract/v2"
 )
 
 func main() {
-	fmt.Println("Test")
+	fmt.Println("Hello from a Go OCR client!")
+
+	tessClient := gosseract.NewClient()
+	defer tessClient.Close()
+	tessClient.SetImage("./images/repo_screenshot.png")
+	text, err := tessClient.Text()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("Recognized text:\n%v\n", text)
 }
